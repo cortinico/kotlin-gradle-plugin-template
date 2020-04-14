b0b78d7c3cb70951a2ea603754cfd3ada8963cb9
Nicola Corti <corti.nico@gmail.com>
2020-04-14 22:18:08 +0200

    Version to 0.1.0

--- plugin-build/buildSrc/src/main/java/Coordinates.kt
+++ plugin-build/buildSrc/src/main/java/Coordinates.kt
@@ -1,15 +1,15 @@
 object PluginCoordinates {
     const val ID = "com.ncorti.kotlin.gradle.template.plugin"
     const val GROUP = "com.ncorti.kotlin.gradle.template"
-    const val VERSION = "1.0.0"
+    const val VERSION = "0.1.0"
     const val IMPLEMENTATION_CLASS = "com.ncorti.kotlin.gradle.template.plugin.TemplatePlugin"
 }
 
 object PluginBundle {
     const val VCS = "https://github.com/cortinico/kotlin-gradle-plugin-template"
     const val WEBSITE = "https://github.com/cortinico/kotlin-gradle-plugin-template"
-    const val DESCRIPTION = "TODO Please Edit your description before publishing"
-    const val DISPLAY_NAME = "Gradle Plugin from Template"
+    const val DESCRIPTION = "An empty Gradle plugin created from a template"
+    const val DISPLAY_NAME = "An empty Gradle Plugin from a template"
     val TAGS = listOf(
         "plugin",
         "gradle",
