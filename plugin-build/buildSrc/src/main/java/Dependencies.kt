665b222f6b49216b23d20abdd687970e42ccb837
Nicola Corti <corti.nico@gmail.com>
2020-04-25 01:12:01 +0200

    Kotlin to 1.3.72 in the composite build (#3)

--- plugin-build/buildSrc/src/main/java/Dependencies.kt
+++ plugin-build/buildSrc/src/main/java/Dependencies.kt
@@ -5,7 +5,7 @@ object Versions {
 
 object BuildPluginsVersion {
     const val DETEKT = "1.7.4"
-    const val KOTLIN = "1.3.71"
+    const val KOTLIN = "1.3.72"
     const val KTLINT = "9.2.1"
     const val PLUGIN_PUBLISH = "0.11.0"
     const val VERSIONS_PLUGIN = "0.28.0"
