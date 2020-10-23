eaca1ac96fefb0626b32e8bf1eedacce67700cd4
Nicola Corti <corti.nico@gmail.com>
2020-10-23 22:49:33 +0200

    Bumping multiple dependencies

--- plugin-build/buildSrc/src/main/java/Dependencies.kt
+++ plugin-build/buildSrc/src/main/java/Dependencies.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val JUNIT = "4.13"
+    const val JUNIT = "4.13.1"
     const val KTLINT = "0.39.0"
 }
 
@@ -7,8 +7,8 @@ object BuildPluginsVersion {
     const val DETEKT = "1.14.2"
     const val KOTLIN = "1.3.72"
     const val KTLINT = "9.4.1"
-    const val PLUGIN_PUBLISH = "0.11.0"
-    const val VERSIONS_PLUGIN = "0.28.0"
+    const val PLUGIN_PUBLISH = "0.12.0"
+    const val VERSIONS_PLUGIN = "0.33.0"
 }
 
 object TestingLib {
