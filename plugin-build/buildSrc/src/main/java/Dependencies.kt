45e28046262340f3f74afd4d112dde2ae53a4694
Nicola Corti <corti.nico@gmail.com>
2021-03-05 19:47:38 +0100

    Kotlin to 1.4.x and beyond (#13)

--- plugin-build/buildSrc/src/main/java/Dependencies.kt
+++ plugin-build/buildSrc/src/main/java/Dependencies.kt
@@ -1,14 +1,14 @@
 object Versions {
-    const val JUNIT = "4.13.1"
-    const val KTLINT = "0.39.0"
+    const val JUNIT = "4.13.2"
+    const val KTLINT = "0.40.0"
 }
 
 object BuildPluginsVersion {
-    const val DETEKT = "1.14.2"
-    const val KOTLIN = "1.3.72"
+    const val DETEKT = "1.15.0"
+    const val KOTLIN = "1.4.21"
     const val KTLINT = "9.4.1"
-    const val PLUGIN_PUBLISH = "0.12.0"
-    const val VERSIONS_PLUGIN = "0.33.0"
+    const val PLUGIN_PUBLISH = "0.13.0"
+    const val VERSIONS_PLUGIN = "0.36.0"
 }
 
 object TestingLib {
