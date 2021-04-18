be45c446ce25c6d39d209b9dd986b39966ae3078
Nicola Corti <corti.nico@gmail.com>
2021-04-18 15:29:32 +0200

    Gradle to 7.0 and more (#19)

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,13 +1,13 @@
 object Versions {
     const val JUNIT = "4.13.2"
-    const val KTLINT = "0.40.0"
+    const val KTLINT = "0.41.0"
 }
 
 object BuildPluginsVersion {
-    const val DETEKT = "1.15.0"
-    const val KOTLIN = "1.4.21"
-    const val KTLINT = "9.4.1"
-    const val VERSIONS_PLUGIN = "0.36.0"
+    const val DETEKT = "1.16.0"
+    const val KOTLIN = "1.4.32"
+    const val KTLINT = "10.0.0"
+    const val VERSIONS_PLUGIN = "0.38.0"
 }
 
 object TestingLib {
