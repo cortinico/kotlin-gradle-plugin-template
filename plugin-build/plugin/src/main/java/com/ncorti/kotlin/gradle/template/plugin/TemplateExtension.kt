47002147d0ccd9d7ba2498c7e201105bb7a24708
Nicola Corti <corti.nico@gmail.com>
2021-04-24 10:28:55 +0200

    Add comments on mandatory values (#23)

--- plugin-build/plugin/src/main/java/com/ncorti/kotlin/gradle/template/plugin/TemplateExtension.kt
+++ plugin-build/plugin/src/main/java/com/ncorti/kotlin/gradle/template/plugin/TemplateExtension.kt
@@ -12,8 +12,11 @@ abstract class TemplateExtension @Inject constructor(project: Project) {
 
     private val objects = project.objects
 
+    // Example of a property that is mandatory. The task will
+    // fail if this property is not set as is annotated with @Optional.
     val message: Property<String> = objects.property(String::class.java)
 
+    // Example of a property that is optional.
     val tag: Property<String> = objects.property(String::class.java)
 
     // Example of a property with a default set with .convention
