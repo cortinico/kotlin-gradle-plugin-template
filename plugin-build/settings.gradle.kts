d9b03630209614ddf6c3141e62a2123d3493358a
Goooler <wangzongler@gmail.com>
2022-07-30 17:52:09 +0800

    Enable gradle-enterprise (#52)

--- plugin-build/settings.gradle.kts
+++ plugin-build/settings.gradle.kts
@@ -18,6 +18,19 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
+        publishOnFailure()
+    }
+}
+
 rootProject.name = ("com.ncorti.kotlin.gradle.template")
 
 include(":plugin")
