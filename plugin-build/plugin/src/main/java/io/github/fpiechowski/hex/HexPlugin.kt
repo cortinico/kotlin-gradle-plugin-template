205b7f3aee5e8a242d7e9aafcc0b8581b046e17e
Filip Piechowski <filip.piechowski@schibsted.com>
2023-02-05 11:28:14 +0100

    hex

--- plugin-build/plugin/src/main/java/io/github/fpiechowski/hex/HexPlugin.kt
+++ plugin-build/plugin/src/main/java/io/github/fpiechowski/hex/HexPlugin.kt
@@ -11,6 +11,7 @@ abstract class HexPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         // Add the 'template' extension object
         val extension = project.extensions.create(EXTENSION_NAME, HexExtension::class.java, project)
+
         with(project.extensions.getByType(JavaPluginExtension::class.java)) {
 
             sourceSets.run {
@@ -55,8 +56,6 @@ abstract class HexPlugin : Plugin<Project> {
                 }
             }
         }
-
-
 
         // Add a task that uses configuration from the extension object
         project.tasks.register(TASK_NAME, GeneratePlantUML::class.java) {
