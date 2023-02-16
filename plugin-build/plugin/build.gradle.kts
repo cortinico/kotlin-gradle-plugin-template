8ed15d492cd4e2b09296ddffe836d8395b5f6e94
Nicola Corti <corti.nico@gmail.com>
2023-02-16 12:37:13 +0100

    Handle bump to Gradle 8

--- plugin-build/plugin/build.gradle.kts
+++ plugin-build/plugin/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm")
     `java-gradle-plugin`
@@ -16,23 +18,28 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
 gradlePlugin {
     plugins {
         create(property("ID").toString()) {
             id = property("ID").toString()
             implementationClass = property("IMPLEMENTATION_CLASS").toString()
             version = property("VERSION").toString()
+            description = property("DESCRIPTION").toString()
             displayName = property("DISPLAY_NAME").toString()
+            tags.set(listOf("plugin", "gradle", "sample", "template"))
         }
     }
 }
 
-// Configuration Block for the Plugin Marker artifact on Plugin Central
-pluginBundle {
-    website = property("WEBSITE").toString()
-    vcsUrl = property("VCS_URL").toString()
-    description = property("DESCRIPTION").toString()
-    tags = listOf("plugin", "gradle", "sample", "template")
+gradlePlugin {
+    website.set(property("WEBSITE").toString())
+    vcsUrl.set(property("VCS_URL").toString())
 }
 
 tasks.create("setupPluginUploadFromEnvironment") {
